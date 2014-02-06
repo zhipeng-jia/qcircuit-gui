5286bfb49371018bbbcccb86cd47b698f4a7a219
zhipeng-jia <blue.boy.in.china@gmail.com>
2014-02-06 14:49:24 +0800

    Delete database.yml. Minor change in application.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,42 +13,42 @@
 //= require jquery-1.11.0.js
 //= require bootstrap.js
 //= require jcanvas.js
-//= require ./global.js.coffee
+//= require global.js.coffee
 
-//= require ./qcircuit_drawing/basic_drawing.js.coffee
-//= require ./qcircuit_drawing/base_component.js.coffee
-//= require ./qcircuit_drawing/latex_code_image.js.coffee
-//= require ./qcircuit_drawing/multi_row_component.js.coffee
+//= require qcircuit_drawing/basic_drawing.js.coffee
+//= require qcircuit_drawing/base_component.js.coffee
+//= require qcircuit_drawing/latex_code_image.js.coffee
+//= require qcircuit_drawing/multi_row_component.js.coffee
 
-//= require ./qcircuit_drawing/quantum_wire.js.coffee
-//= require ./qcircuit_drawing/classical_wire.js.coffee
-//= require ./qcircuit_drawing/control.js.coffee
-//= require ./qcircuit_drawing/gate.js.coffee
-//= require ./qcircuit_drawing/left_stick.js.coffee
-//= require ./qcircuit_drawing/right_stick.js.coffee
-//= require ./qcircuit_drawing/measure.js.coffee
-//= require ./qcircuit_drawing/measure_d.js.coffee
-//= require ./qcircuit_drawing/measure_tab.js.coffee
-//= require ./qcircuit_drawing/meter.js.coffee
-//= require ./qcircuit_drawing/qswap.js.coffee
-//= require ./qcircuit_drawing/target_gate.js.coffee
+//= require qcircuit_drawing/quantum_wire.js.coffee
+//= require qcircuit_drawing/classical_wire.js.coffee
+//= require qcircuit_drawing/control.js.coffee
+//= require qcircuit_drawing/gate.js.coffee
+//= require qcircuit_drawing/left_stick.js.coffee
+//= require qcircuit_drawing/right_stick.js.coffee
+//= require qcircuit_drawing/measure.js.coffee
+//= require qcircuit_drawing/measure_d.js.coffee
+//= require qcircuit_drawing/measure_tab.js.coffee
+//= require qcircuit_drawing/meter.js.coffee
+//= require qcircuit_drawing/qswap.js.coffee
+//= require qcircuit_drawing/target_gate.js.coffee
 
-//= require ./qcircuit_drawing/grid.js.coffee
-//= require ./qcircuit_drawing/circuit.js.coffee
+//= require qcircuit_drawing/grid.js.coffee
+//= require qcircuit_drawing/circuit.js.coffee
 
-//= require ./qcircuit_editing/helper.js.coffee
-//= require ./qcircuit_editing/select_row_or_column.js.coffee
-//= require ./qcircuit_editing/select_row.js.coffee
+//= require qcircuit_editing/helper.js.coffee
+//= require qcircuit_editing/select_row_or_column.js.coffee
+//= require qcircuit_editing/select_row.js.coffee
 
-//= require ./qcircuit_editing/add_wire.js.coffee
+//= require qcircuit_editing/add_wire.js.coffee
 //= require qcircuit_editing/add_multi_row_component.js.coffee
-//= require ./qcircuit_editing/add_single_cell_component.js.coffee
+//= require qcircuit_editing/add_single_cell_component.js.coffee
 
-//= require ./qcircuit_editing/add_row.js.coffee
-//= require ./qcircuit_editing/add_column.js.coffee
-//= require ./qcircuit_editing/delete_row.js.coffee
-//= require ./qcircuit_editing/delete_column.js.coffee
-//= require ./qcircuit_editing/erase_cell_content.js.coffee
-//= require ./qcircuit_editing/erase_wire.js.coffee
+//= require qcircuit_editing/add_row.js.coffee
+//= require qcircuit_editing/add_column.js.coffee
+//= require qcircuit_editing/delete_row.js.coffee
+//= require qcircuit_editing/delete_column.js.coffee
+//= require qcircuit_editing/erase_cell_content.js.coffee
+//= require qcircuit_editing/erase_wire.js.coffee
 
-//= require ./qcircuit_editing/editing_interface.js.coffee
+//= require qcircuit_editing/editing_interface.js.coffee
